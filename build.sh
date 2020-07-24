775a5e2cc9f887a6c43f1a18baa25c36bf542f4b
Rahul Menon <rahul@rmenn.in>
2020-07-25 02:36:58 +0530

    glibc added to scratch alpine

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ALPINE_ARCH=x86_64
+ALPINE_VERSION=3.12.0
+ALPINE_PARENT=$(echo $ALPINE_VERSION | cut -d '.' -f1,2)
+ALPINE_FILENAME=alpine-minirootfs-${ALPINE_VERSION}-${ALPINE_ARCH}.tar.gz
+echo ${ALPINE_PARENT}
+
+BASE_URL=http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_PARENT}/releases/${ALPINE_ARCH}
+wget $BASE_URL/$ALPINE_FILENAME
+docker build -t rmenn/alpine-glibc:${ALPINE_VERSION} --build-arg ALPINE_VERSION=${ALPINE_VERSION} --build-arg ALPINE_ARCH=${ALPINE_ARCH} .
+rm ${ALPINE_FILENAME}
+docker push rmenn/alpine-glibc:${ALPINE_VERSION}
+docker rmi rmenn/alpine-glibc:${ALPINE_VERSION}
